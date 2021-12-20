f3589066cffff9f0f4943820b5021de874ab886f
jayteealao_ec2 <jayteealao@gmail.com>
2021-12-20 17:38:36 +0000

    changed dataclass

--- app/src/main/java/com/gadsag01/findhealth/data/Hospital.kt
+++ app/src/main/java/com/gadsag01/findhealth/data/Hospital.kt
@@ -1,12 +1,8 @@
 package com.gadsag01.findhealth.data
 
-import com.google.android.libraries.places.api.model.OpeningHours
-
-data class Hospital(
+data class HospitalBasic(
     val name: String,
-    val formattedAddress: String,
+    val formattedAddress: String?,
     val ID: String,
-    val formatted_phone_number: String,
-    val googleUrl: String,
-    val openingHours: OpeningHours,
+    val rating: Float?,
 )
