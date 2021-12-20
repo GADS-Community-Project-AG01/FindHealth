92d7b00ba249b448c86a76d04b26ee222acc1363
jayteealao_ec2 <jayteealao@gmail.com>
2021-12-20 21:09:31 +0000

    added an hospitalfull details data class

--- app/src/main/java/com/gadsag01/findhealth/data/Hospital.kt
+++ app/src/main/java/com/gadsag01/findhealth/data/Hospital.kt
@@ -4,9 +4,21 @@ import com.google.maps.model.Geometry
 import com.google.maps.model.OpeningHours
 import java.net.URL
 
-data class HospitalBasic(
+data class HospitalBasic(val placeId: String,
     val name: String,
     val formattedAddress: String?,
-    val ID: String,
     val rating: Float?,
 )
+
+data class HospitalFull(
+    val placedId: String,
+    val name: String,
+    val formattedAddress: String?,
+    val geometry: Geometry,
+    val phoneNumber: String?,
+    val openingHours: OpeningHours?,
+    val rating: Float?,
+    val userRatingsTotal: Int,
+    val googleUrl: URL,
+    val website: URL?
+    )
