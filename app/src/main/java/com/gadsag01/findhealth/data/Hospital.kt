f38e1bec707fc8bd5f03ab428b48e0a91189ba91
jayteealao_ec2 <jayteealao@gmail.com>
2021-12-20 18:14:37 +0000

    permission request using easypermissions

--- app/src/main/java/com/gadsag01/findhealth/data/Hospital.kt
+++ app/src/main/java/com/gadsag01/findhealth/data/Hospital.kt
@@ -1,5 +1,9 @@
 package com.gadsag01.findhealth.data
 
+import com.google.maps.model.Geometry
+import com.google.maps.model.OpeningHours
+import java.net.URL
+
 data class HospitalBasic(
     val name: String,
     val formattedAddress: String?,
